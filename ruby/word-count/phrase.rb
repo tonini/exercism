db2364d8a57411cc848a2c6dc9fe9fd7de6cd5f5
Samuel Tonini <tonini.samuel@gmail.com>
2013-08-12 13:41:32 +0200

    refactore and add word scanner to handle regex logic

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -3,33 +3,77 @@ class Phrase
   attr_reader :storage
 
   def initialize(phrase)
-    @storage = WordStorage.new(parse(phrase))
+    scanner = WordScanner.new(phrase)
+    words = scanner.split!
+    @storage = WordStorage.new(words)
   end
 
   def word_count
-    storage.word_count
+    storage.words
   end
 
-  def parse(phrase)
-    phrase.
-      gsub(/(?!,)[[:punct:]\^$]/, '').
-      gsub(/\s+/, ' ').
-      gsub(/\s+/, ',').
-      split(',').
-      delete_if { |entry| entry.empty? }.map { |word| word.downcase }
+end
+
+class WordScanner
+
+  attr_accessor :phrase
+
+  PUNCTUATION = /(?!,)[[:punct:]\^$]/
+  DELIMITER   = ','
+  WHITESPACE  = ' '
+
+  def initialize(phrase)
+    @phrase = phrase
+  end
+
+  def split!
+    prepare_phrase
+    split_phrase
+  end
+
+  private
+
+  def prepare_phrase
+    remove_punctuation!
+    remove_whitespace!
+    set_delimitier!
+  end
+
+  def split_phrase
+    self.phrase = phrase.split(DELIMITER).delete_if do |entry|
+      entry.empty?
+    end.map do |word|
+      word.downcase
+    end
+  end
+
+  def remove_punctuation!
+    self.phrase = phrase.gsub(PUNCTUATION, '')
+  end
+
+  def remove_whitespace!
+    self.phrase = phrase.gsub(/\s+/, WHITESPACE)
+  end
+
+  def set_delimitier!
+    self.phrase = phrase.gsub(/\s+/, DELIMITER)
   end
 
 end
 
 class WordStorage
 
+  WORD_INC_COUNT = 1
+
   def initialize(words)
     @words = save(words)
   end
 
-  def word_count
+  def words
     storage
   end
+
+  private
 
   def save(words)
     words.each { |word| store(word) }
@@ -37,9 +81,9 @@ class WordStorage
 
   def store(word)
     storage[word] = if storage.has_key?(word)
-                      storage[word] + 1
+                      storage[word] + WORD_INC_COUNT
                     else
-                      1
+                      WORD_INC_COUNT
                     end
   end
 
